a5fac8cf3f3cec5df4a0258a701217fa59f0eb93
1155092080 <1155092080@link.cuhk.edu.hk>
2022-11-09 14:05:13 +0800

    add plotting python function

--- cppfiles/plotinfo.py
+++ cppfiles/plotinfo.py
@@ -5,9 +5,11 @@ file_name = ["./Outfile/Star_WENO_Density_NM.dat", "./Outfile/Star_WENO_Density_
 col_name = [["step","rhoNM"], ["step","rhoDM"]]
 
 ## Read files
-dfNM = pd.read_csv(file_name[0],delim_whitespace=True, names=col_name[0], header=1, engine='python')
-dfDM = pd.read_csv(file_name[1],delim_whitespace=True, names=col_name[1], header=1, engine='python')
+dfNM = pd.read_csv(file_name[0],delim_whitespace=True, names=col_name[0], skiprows=1, header=None, engine='python')
+dfDM = pd.read_csv(file_name[1],delim_whitespace=True, names=col_name[1], skiprows=1, header=None, engine='python')
 # Concatenate data according to stepnumber
 df = pd.merge(dfDM, dfNM, on='step')
 
 print(df)
+
+df.plot(x='step', y=['rhoDM','rhoNM'],logy=True)
